94849811a40426cb178c8ac52aad6e9f68de8cbb
James Stephen Bulfa <jamesbulfa55@gmail.com>
2023-11-21 23:46:35 +0800

    initial

--- app/src/main/java/com/bulfa/activity5/MainActivity.kt
+++ app/src/main/java/com/bulfa/activity5/MainActivity.kt
@@ -3,7 +3,6 @@ package com.bulfa.activity5
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
